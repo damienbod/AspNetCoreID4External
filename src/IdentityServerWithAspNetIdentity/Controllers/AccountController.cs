4a8ba1e5cd1f23be6950aca12dd86dfe147150a7
damienbod <damien_bod@hotmail.com>
2018-05-01 16:09:18 +0200

    Adding logout model again

--- src/IdentityServerWithAspNetIdentity/Controllers/AccountController.cs
+++ src/IdentityServerWithAspNetIdentity/Controllers/AccountController.cs
@@ -13,6 +13,11 @@ using Microsoft.Extensions.Options;
 using IdentityServerWithAspNetIdentity.Models;
 using IdentityServerWithAspNetIdentity.Models.AccountViewModels;
 using IdentityServerWithAspNetIdentity.Services;
+using IdentityModel;
+using IdentityServer4.Extensions;
+using IdentityServer4;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
 
 namespace IdentityServerWithAspNetIdentity.Controllers
 {
@@ -25,16 +30,29 @@ namespace IdentityServerWithAspNetIdentity.Controllers
         private readonly IEmailSender _emailSender;
         private readonly ILogger _logger;
 
+        private readonly IIdentityServerInteractionService _interaction;
+        private readonly IClientStore _clientStore;
+        private readonly IPersistedGrantService _persistedGrantService;
+
         public AccountController(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
             IEmailSender emailSender,
-            ILogger<AccountController> logger)
+            ILogger<AccountController> logger,
+            IPersistedGrantService persistedGrantService,
+            IIdentityServerInteractionService interaction,
+            IClientStore clientStore
+        )
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _emailSender = emailSender;
             _logger = logger;
+
+            _persistedGrantService = persistedGrantService;
+            _interaction = interaction;
+            _clientStore = clientStore;
+
         }
 
         [TempData]
@@ -241,15 +259,88 @@ namespace IdentityServerWithAspNetIdentity.Controllers
             return View(model);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Logout()
+        /// <summary>
+        /// Show logout page
+        /// </summary>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Logout(string logoutId)
         {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
-            return RedirectToAction(nameof(HomeController.Index), "Home");
+            if (User.Identity.IsAuthenticated == false)
+            {
+                // if the user is not authenticated, then just show logged out page
+                return await Logout(new LogoutViewModel { LogoutId = logoutId });
+            }
+
+            var context = await _interaction.GetLogoutContextAsync(logoutId);
+            if (context?.ShowSignoutPrompt == false)
+            {
+                // it's safe to automatically sign-out
+                return await Logout(new LogoutViewModel { LogoutId = logoutId });
+            }
+
+            // show the logout prompt. this prevents attacks where the user
+            // is automatically signed out by another malicious web page.
+            var vm = new LogoutViewModel
+            {
+                LogoutId = logoutId
+            };
+
+            return View(vm);
         }
 
+        /// <summary>
+        /// Handle logout page postback
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public async Task<IActionResult> Logout(LogoutViewModel model)
+        {
+            var idp = User?.FindFirst(JwtClaimTypes.IdentityProvider)?.Value;
+            var subjectId = HttpContext.User.Identity.GetSubjectId();
+
+            if (idp != null && idp != IdentityServerConstants.LocalIdentityProvider)
+            {
+                if (model.LogoutId == null)
+                {
+                    // if there's no current logout context, we need to create one
+                    // this captures necessary info from the current logged in user
+                    // before we signout and redirect away to the external IdP for signout
+                    model.LogoutId = await _interaction.CreateLogoutContextAsync();
+                }
+
+                string url = "/Account/Logout?logoutId=" + model.LogoutId;
+                try
+                {
+                    await _signInManager.SignOutAsync();
+                    // await HttpContext.Authentication.SignOutAsync(idp, new AuthenticationProperties { RedirectUri = url });
+                }
+                catch (NotSupportedException)
+                {
+                }
+            }
+
+            // delete authentication cookie
+            await _signInManager.SignOutAsync();
+
+            // set this so UI rendering sees an anonymous user
+            HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity());
+
+            // get context information (client name, post logout redirect URI and iframe for federated signout)
+            var logout = await _interaction.GetLogoutContextAsync(model.LogoutId);
+
+            var vm = new LoggedOutViewModel
+            {
+                PostLogoutRedirectUri = logout?.PostLogoutRedirectUri,
+                ClientName = logout?.ClientId,
+                SignOutIframeUrl = logout?.SignOutIFrameUrl
+            };
+
+            await _persistedGrantService.RemoveAllGrantsAsync(subjectId, "angular2client");
+
+            return View("LoggedOut", vm);
+        }
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
---
+++ src/IdentityServerWithAspNetIdentity/Models/AccountViewModels/LogoutViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace IdentityServerWithAspNetIdentity.Models.AccountViewModels
+{
+    public class LogoutViewModel
+    {
+        public string LogoutId { get; set; }
+    }
+}
