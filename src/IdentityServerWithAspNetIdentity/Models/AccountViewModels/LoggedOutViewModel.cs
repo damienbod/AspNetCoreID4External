073074cff2baef26da9d44eb971bf2dd7965c97c
damienbod <damien_bod@hotmail.com>
2018-05-01 15:50:44 +0200

    Adding a logged out view

---
+++ src/IdentityServerWithAspNetIdentity/Models/AccountViewModels/LoggedOutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerWithAspNetIdentity.Models.AccountViewModels
+{
+
+    public class LoggedOutViewModel
+    {
+        public string PostLogoutRedirectUri { get; set; }
+        public string ClientName { get; set; }
+        public string SignOutIframeUrl { get; set; }
+    }
+}
