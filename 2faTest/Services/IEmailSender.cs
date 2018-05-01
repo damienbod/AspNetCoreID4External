8fe11112fe48040e2cf32b0b55d0701100943dc2
damienbod <damien_bod@hotmail.com>
2018-05-01 14:48:45 +0200

    test sfa

---
+++ 2faTest/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2faTest.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
