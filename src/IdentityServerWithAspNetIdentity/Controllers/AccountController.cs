e441f95552aafff44ba4ebba57e6026d38794e70
damienbod <damien_bod@hotmail.com>
2018-05-01 16:57:29 +0200

    code clean up

--- src/IdentityServerWithAspNetIdentity/Controllers/AccountController.cs
+++ src/IdentityServerWithAspNetIdentity/Controllers/AccountController.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using IdentityServerWithAspNetIdentity.Models;
 using IdentityServerWithAspNetIdentity.Models.AccountViewModels;
 using IdentityServerWithAspNetIdentity.Services;
