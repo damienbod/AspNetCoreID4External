0b8fbb1706a85a61c78f83a93917da8b7e836087
damienbod <damien_bod@hotmail.com>
2020-05-03 12:09:21 +0200

    updating angular app

--- src/AngularClient/angularApp/app/app.module.ts
+++ src/AngularClient/angularApp/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AppRoutes } from './app.routes';
-import { CoreModule } from './core/core.module';
-import { HomeModule } from './home/home.module';
-import { NewsModule } from './news/news.module';
+import { routing } from './app.routes';
 import { Configuration } from './app.constants';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { DataEventRecordsModule } from './dataeventrecords/dataeventrecords.module';
-import { SharedModule } from './shared/shared.module';
-
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { HomeComponent } from './home/home.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 import { AuthModule, OidcConfigService } from 'angular-auth-oidc-client';
 import { map, switchMap } from 'rxjs/operators';
 import { L10nConfig, L10nLoader, TranslationModule, StorageStrategy, ProviderType } from 'angular-l10n';
@@ -109,3 +105,4 @@ export class AppModule {
 
         console.log('APP STARTING');
     }
+}
