cc320c08c85c9c9946f9a47837df7decdeacabd1
damienbod <damien_bod@hotmail.com>
2020-07-10 23:52:25 +0200

    readme

--- src/IdentityServerWithAspNetIdentity/IdentityWithAdditionalClaimsProfileService.cs
+++ src/IdentityServerWithAspNetIdentity/IdentityWithAdditionalClaimsProfileService.cs
@@ -31,18 +31,8 @@ namespace StsServerIdentity
             var principal = await _claimsFactory.CreateAsync(user);
 
             var claims = principal.Claims.ToList();
-
             claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
-            
-
             claims.Add(new Claim(JwtClaimTypes.GivenName, user.UserName));
-            //new Claim(JwtClaimTypes.Role, "admin"),
-            //new Claim(JwtClaimTypes.Role, "dataEventRecords.admin"),
-            //new Claim(JwtClaimTypes.Role, "dataEventRecords.user"),
-            //new Claim(JwtClaimTypes.Role, "dataEventRecords"),
-            //new Claim(JwtClaimTypes.Role, "securedFiles.user"),
-            //new Claim(JwtClaimTypes.Role, "securedFiles.admin"),
-            //new Claim(JwtClaimTypes.Role, "securedFiles")
 
             if (user.IsAdmin)
             {
