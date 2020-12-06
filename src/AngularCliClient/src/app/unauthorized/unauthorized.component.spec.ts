c0cb18a6b73c984eeb0f2f502626495c6d33f5da
damienbod <damien_bod@hotmail.com>
2020-12-06 07:20:00 +0100

    update Angular CLI

--- src/AngularCliClient/src/app/unauthorized/unauthorized.component.spec.ts
+++ src/AngularCliClient/src/app/unauthorized/unauthorized.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UnauthorizedComponent } from './unauthorized.component';
 
@@ -6,7 +6,7 @@ describe('UnauthorizedComponent', () => {
   let component: UnauthorizedComponent;
   let fixture: ComponentFixture<UnauthorizedComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UnauthorizedComponent ]
     })
