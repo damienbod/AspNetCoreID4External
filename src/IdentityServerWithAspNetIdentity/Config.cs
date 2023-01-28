3986521bd41fda9529e1bf5a5f0d9e12308c819d
damienbod <damien_bod@hotmail.com>
2023-01-28 23:59:39 +0100

    code clean up

--- src/IdentityServerWithAspNetIdentity/Config.cs
+++ src/IdentityServerWithAspNetIdentity/Config.cs
@@ -4,86 +4,85 @@
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
-namespace StsServerIdentity
+namespace StsServerIdentity;
+
+public class Config
 {
-    public class Config
+    public static IEnumerable<IdentityResource> GetIdentityResources()
     {
-        public static IEnumerable<IdentityResource> GetIdentityResources()
+        return new List<IdentityResource>
         {
-            return new List<IdentityResource>
-            {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                new IdentityResources.Email()
-            };
-        }
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email()
+        };
+    }
 
-        public static IEnumerable<ApiScope> GetApiScopes()
+    public static IEnumerable<ApiScope> GetApiScopes()
+    {
+        return new List<ApiScope>
         {
-            return new List<ApiScope>
-            {
-                new ApiScope("dataEventRecords", "Scope for the dataEventRecords")
-            };
-        }
+            new ApiScope("dataEventRecords", "Scope for the dataEventRecords")
+        };
+    }
 
-        public static IEnumerable<ApiResource> GetApiResources()
+    public static IEnumerable<ApiResource> GetApiResources()
+    {
+        return new List<ApiResource>
         {
-            return new List<ApiResource>
+            new ApiResource("dataEventRecordsApi")
             {
-                new ApiResource("dataEventRecordsApi")
+                ApiSecrets = { new Secret("dataEventRecordsSecret".Sha256()) },
+                Scopes = { "dataEventRecords" },
+                UserClaims = { "role", "admin", "user", "dataEventRecords", "dataEventRecords.admin", "dataEventRecords.user" }
+            }
+        };
+    }
+
+    public static IEnumerable<Client> GetClients()
+    {
+        // client credentials client
+        return new List<Client>
+        {
+            new Client
+            {
+                ClientName = "angularclient",
+                ClientId = "angularclient",
+                AccessTokenType = AccessTokenType.Reference,
+                AccessTokenLifetime = 330,// 120 seconds, default 60 minutes
+                IdentityTokenLifetime = 300,
+
+                RequireClientSecret = false,
+                AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = true,
+
+                AllowOfflineAccess = true,
+                RefreshTokenUsage = TokenUsage.OneTimeOnly,
+
+                AllowAccessTokensViaBrowser = true,
+                RedirectUris = new List<string>
                 {
-                    ApiSecrets = { new Secret("dataEventRecordsSecret".Sha256()) },
-                    Scopes = { "dataEventRecords" },
-                    UserClaims = { "role", "admin", "user", "dataEventRecords", "dataEventRecords.admin", "dataEventRecords.user" }
-                }
-            };
-        }
+                    "https://localhost:4200"
 
-        public static IEnumerable<Client> GetClients()
-        {
-            // client credentials client
-            return new List<Client>
-            {
-                new Client
+                },
+                PostLogoutRedirectUris = new List<string>
                 {
-                    ClientName = "angularclient",
-                    ClientId = "angularclient",
-                    AccessTokenType = AccessTokenType.Reference,
-                    AccessTokenLifetime = 330,// 120 seconds, default 60 minutes
-                    IdentityTokenLifetime = 300,
-
-                    RequireClientSecret = false,
-                    AllowedGrantTypes = GrantTypes.Code,
-                    RequirePkce = true,
-
-                    AllowOfflineAccess = true,
-                    RefreshTokenUsage = TokenUsage.OneTimeOnly,
-
-                    AllowAccessTokensViaBrowser = true,
-                    RedirectUris = new List<string>
-                    {
-                        "https://localhost:4200"
-
-                    },
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "https://localhost:4200",
-                        "https://localhost:4200/unauthorized"
-                    },
-                    AllowedCorsOrigins = new List<string>
-                    {
-                        "https://localhost:4200"
-                    },
-                    AllowedScopes = new List<string>
-                    {
-                        "openid",
-                        "dataEventRecords",
-                        "role",
-                        "profile",
-                        "email"
-                    }
+                    "https://localhost:4200",
+                    "https://localhost:4200/unauthorized"
+                },
+                AllowedCorsOrigins = new List<string>
+                {
+                    "https://localhost:4200"
+                },
+                AllowedScopes = new List<string>
+                {
+                    "openid",
+                    "dataEventRecords",
+                    "role",
+                    "profile",
+                    "email"
                 }
-            };
-        }
+            }
+        };
     }
 }
