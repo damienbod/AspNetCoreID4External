30f3d0079d8e694ee4daa84a96a670c888f6bddc
damienbod <damien_bod@hotmail.com>
2023-01-29 16:13:28 +0100

    clean up

--- src/IdentityProvider/Services/Certificate/KeyVaultCertificateService.cs
+++ src/IdentityProvider/Services/Certificate/KeyVaultCertificateService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
+﻿using System.Security.Cryptography.X509Certificates;
 using Azure.Security.KeyVault.Certificates;
 using Azure.Security.KeyVault.Secrets;
 
