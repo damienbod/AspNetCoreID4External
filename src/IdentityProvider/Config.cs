aedf0dad78ed816f4d6a25758be6517c5105386e
damienbod <damien_bod@hotmail.com>
2023-01-29 08:59:23 +0100

    update client settings

--- src/IdentityProvider/Config.cs
+++ src/IdentityProvider/Config.cs
@@ -11,22 +11,12 @@ public static class Config
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
             new IdentityResources.Email(),
-            new IdentityResource("dataeventrecordsir",new []{ "role", "admin", "user", "dataEventRecords", "dataEventRecords.admin" , "dataEventRecords.user" } ),
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
         {
-            new ApiScope("dataEventRecords", "Scope for the dataEventRecords ApiResource",
-                    new List<string> {
-                        "role",
-                        "admin",
-                        "user",
-                        "dataEventRecords",
-                        "dataEventRecords.admin",
-                        "dataEventRecords.user"
-                    }
-            )
+            new ApiScope("dataEventRecords", "Scope for the dataEventRecords")
         };
 
     public static IEnumerable<ApiResource> ApiResources =>
@@ -34,95 +24,52 @@ public static class Config
         {
             new ApiResource("dataEventRecordsApi")
             {
-                ApiSecrets =
-                {
-                    new Secret("dataEventRecordsSecret".Sha256())
-                },
-                Scopes = new List<string> { "dataEventRecords" }
+                ApiSecrets = { new Secret("dataEventRecordsSecret".Sha256()) },
+                Scopes = { "dataEventRecords" },
+                UserClaims = { "role", "admin", "user", "dataEventRecords", "dataEventRecords.admin", "dataEventRecords.user" }
             }
-
         };
 
     public static IEnumerable<Client> Clients => new Client[]
     {
         new Client
-        {
-            ClientName = "angularclient",
-            ClientId = "angularclient",
-            AccessTokenType = AccessTokenType.Jwt,
-            AccessTokenLifetime = 3300,// 330 seconds, default 60 minutes
-            IdentityTokenLifetime = 3000,
+            {
+                ClientName = "angularclient",
+                ClientId = "angularclient",
+                AccessTokenType = AccessTokenType.Reference,
+                AccessTokenLifetime = 330,// 120 seconds, default 60 minutes
+                IdentityTokenLifetime = 300,
 
-            RequireClientSecret = false,
-            AllowedGrantTypes = GrantTypes.Code,
-            RequirePkce = true,
+                RequireClientSecret = false,
+                AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = true,
 
-            AllowAccessTokensViaBrowser = true,
-            AllowOfflineAccess =true,
-            AlwaysIncludeUserClaimsInIdToken = true,
-            RedirectUris = new List<string>
-            {
-                "https://localhost:44311",
-                "https://localhost:44311/silent-renew.html"
+                AllowOfflineAccess = true,
+                RefreshTokenUsage = TokenUsage.OneTimeOnly,
 
-            },
-            PostLogoutRedirectUris = new List<string>
-            {
-                "https://localhost:44311/unauthorized",
-                "https://localhost:44311"
-            },
-            AllowedCorsOrigins = new List<string>
-            {
-                "https://localhost:44311"
-            },
-            AllowedScopes = new List<string>
-            {
-                "openid",
-                "dataEventRecords",
-                "dataeventrecordsscope",
-                "role",
-                "profile",
-                "email"
+                AllowAccessTokensViaBrowser = true,
+                RedirectUris = new List<string>
+                {
+                    "https://localhost:4200"
+
+                },
+                PostLogoutRedirectUris = new List<string>
+                {
+                    "https://localhost:4200",
+                    "https://localhost:4200/unauthorized"
+                },
+                AllowedCorsOrigins = new List<string>
+                {
+                    "https://localhost:4200"
+                },
+                AllowedScopes = new List<string>
+                {
+                    "openid",
+                    "dataEventRecords",
+                    "role",
+                    "profile",
+                    "email"
+                }
             }
-        },
-        new Client
-        {
-            ClientName = "angularclient2",
-            ClientId = "angularclient2",
-            AccessTokenType = AccessTokenType.Jwt,
-            AccessTokenLifetime = 3300,// 330 seconds, default 60 minutes
-            IdentityTokenLifetime = 3000,
-
-            RequireClientSecret = false,
-            AllowedGrantTypes = GrantTypes.Code,
-            RequirePkce = true,
-
-            AllowAccessTokensViaBrowser = true,
-            AllowOfflineAccess =true,
-            AlwaysIncludeUserClaimsInIdToken = true,
-            RedirectUris = new List<string>
-            {
-                "https://localhost:44395",
-                "https://localhost:44395/silent-renew.html"
-            },
-            PostLogoutRedirectUris = new List<string>
-            {
-                "https://localhost:44395/unauthorized",
-                "https://localhost:44395"
-            },
-            AllowedCorsOrigins = new List<string>
-            {
-                "https://localhost:44395"
-            },
-            AllowedScopes = new List<string>
-            {
-                "openid",
-                "dataEventRecords",
-                "dataeventrecordsscope",
-                "role",
-                "profile",
-                "email"
-            }
-        }
     };
 }
