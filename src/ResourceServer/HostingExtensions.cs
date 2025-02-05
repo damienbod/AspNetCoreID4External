8c0fcb1e2021a6d1f642c387b19b8b2db8c05cb3
damienbod <damien_bod@hotmail.com>
2025-02-05 11:39:21 +0100

    code clean up

--- src/ResourceServer/HostingExtensions.cs
+++ src/ResourceServer/HostingExtensions.cs
@@ -1,10 +1,6 @@
-
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.OpenApi;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using NetEscapades.AspNetCore.SecurityHeaders.Infrastructure;
 using ResourceServer.Model;
 using ResourceServer.Repositories;
@@ -141,34 +137,5 @@ internal static class HostingExtensions
         }
 
         return app;
-    }
-
-    internal sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
-    {
-        public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
-        {
-            var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
-            if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
-            {
-                var requirements = new Dictionary<string, OpenApiSecurityScheme>
-                {
-                    ["Bearer"] = new OpenApiSecurityScheme
-                    {
-                        Type = SecuritySchemeType.Http,
-                        Scheme = "bearer", // "bearer" refers to the header name here
-                        In = ParameterLocation.Header,
-                        BearerFormat = "Json Web Token"
-                    }
-                };
-                document.Components ??= new OpenApiComponents();
-                document.Components.SecuritySchemes = requirements;
-            }
-            document.Info = new()
-            {
-                Title = "ResourceServer API Bearer scheme",
-                Version = "v1",
-                Description = "ResourceServer API"
-            };
-        }
     }
 }
